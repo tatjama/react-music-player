6149c871a9fe9aae6c6e78b38e90e62640574864
tatjama <tanja120a@gmail.com>
2020-11-17 08:10:20 +0100

    Sofija music

--- src/data.js
+++ src/data.js
@@ -9,6 +9,16 @@ import notAfraidImage from './images/not-afraid.jpg';
 import notAfraid from './music/Eminem - Not Afraid (Official Video).mp3';
 import loveWayLieImage from './images/love-the-way-you-lie.jpeg';
 import loveWayLie from './music/Eminem - Love The Way You Lie ft. Rihanna.mp3';
+import frozenImage from './images/frozen.jpeg';
+import frozen from './music/FROZENLet It Go Sing-alongOfficial Disney UK.mp3';
+import frozen2Image from './images/frozen2.jpeg';
+import frozen2 from './music/Frozen - All Is Found (From Frozen 2Lyric Video).mp3';
+import friendsImage from './images/friends.jpeg';
+import friends from './music/Marshmello & Anne-Marie - FRIENDS (Lyric Video) OFFICIAL FRIENDZONE ANTHEM.mp3';
+import danceMankeyImage from './images/dance-monkey.jpeg';
+import danceMonkey from './music/Tones and I - Dance Monkey (Lyrics).mp3';
+import heyBrotherImage from './images/hey-brother.jpeg';
+import heyBrother from './music/Avicii - Hey brother.mp3';
 
 function chillHop(){
 
@@ -82,6 +92,51 @@ function chillHop(){
             artist: "Ian Ewing, Strehlow",
             audio: "https://mp3.chillhop.com/serve.php/?mp3=9363",
             color: ["#FCF1D5", "#937084"],
+            id: uuidv4(),
+            active: false
+        },
+        {
+            name: "Hey brother",
+            cover: heyBrotherImage,
+            artist: "Avicci",
+            audio: heyBrother,
+            color: ["#41F3A0", "#599D9B"],
+            id: uuidv4(),
+            active: false
+        },
+        {
+            name: "Dance Monkey",
+            cover: danceMankeyImage,
+            artist: "Tones and I",
+            audio: danceMonkey,
+            color: ["#F5E246", "#E96146"],
+            id: uuidv4(),
+            active: false
+        },
+        {
+            name: "Friends",
+            cover: friendsImage,
+            artist: "Marshmello & Anne-Marie",
+            audio: friends,
+            color: ["#EDDA17", "#982178"],
+            id: uuidv4(),
+            active: false
+        },
+        {
+            name: "Let it go",
+            cover: frozenImage,
+            artist: "Frozen",
+            audio: frozen,
+            color: ["#DEDAC6", "#77B7D0"],
+            id: uuidv4(),
+            active: false
+        },
+        {
+            name: "All is found",
+            cover: frozen2Image,
+            artist: "Frozen 2",
+            audio: frozen2,
+            color: ["#BAC3CD", "#586285"],
             id: uuidv4(),
             active: false
         },
